3a2b5e75048bff03973dff515f7ca0b276c1b27a
SabaKoberidze <koberidzesaba@gmail.com>
2024-09-20 05:33:36 +0400

    production testing

--- projectTrucks/nuxt.config.ts
+++ projectTrucks/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'node-server'
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['@/assets/styles/main.scss'],
@@ -14,4 +17,6 @@ export default defineNuxtConfig({
       exclude: ["/"],
     },
   },
+  
 })
+
